7ee3c4c98e2c2811edb4901d5467b14d19023ff2
akash-268 <aakbajpai268@gmail.com>
2022-04-19 23:48:25 +0530

    Added port

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Hello World')
+// console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+// const port=5000
+
+const app = express()
+
+app.listen(process.env.port,()=>{
+    console.log(`Server started on port ${process.env.port}`)
+})
