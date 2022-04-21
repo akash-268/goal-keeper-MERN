81f07403a025e63eae8e632733a122575a99fbab
akash-268 <aakbajpai268@gmail.com>
2022-04-22 00:21:36 +0530

    Added error handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,7 +3,11 @@ const getGoals = (req,res) => {
 };
 
 const setGoal = (req, res) => {
-  res.status(200).json({ message: "Set goals" })
+  if(!req.body.text){
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
+  res.status(200).json({ message: "Set goal" })
 };
 
 const updateGoal = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const goalRoutes = require('./routes/goalRoutes')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const app = express()
 
+//Middlewares
+app.use(express.json()) //Parsing the body to json
+app.use(express.urlencoded({extended: false})) //Encoding to URL-encoded format
+
+//Routes
 app.use('/api/goals', goalRoutes)
 
+//Error-Handler Middleware
+app.use(errorHandler)
+
+//Servr
 app.listen(process.env.port,()=>{
     console.log(`Server started on port ${process.env.port}`)
 })
