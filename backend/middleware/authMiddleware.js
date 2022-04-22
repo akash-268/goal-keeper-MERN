80878e4634447ab28d3fb20e4b1d252b8c05e457
akash-268 <aakbajpai268@gmail.com>
2022-04-22 15:23:37 +0530

    Generate JWT

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../model/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      //Get token from Header
+      token = req.headers.authorization.split(" ")[1];
+
+      //Verify Token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      //Get user from the token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (err) {
+      console.log(err);
+      res.status(401);
+      throw new Error("Not autohorized");
+    }
+  }
+
+  //If No token found
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, No token found");
+  }
+});
+
+module.exports = protect;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser,  getMe, loginUser } = require("../controllers/userController.js");
+const {
+  registerUser,
+  getMe,
+  loginUser,
+} = require("../controllers/userController.js");
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", registerUser);
 
-router.get("/me", getMe);
+router.get("/me", protect, getMe);
 
-router.post('/login',loginUser);
+router.post("/login", loginUser);
 
 module.exports = router;
